865f7166168063d9b9d1b2a54e44508a3f54f051
Kevin <mccreeper1319@icloud.com>
2026-04-30 11:57:37 -0400

    update paper api dependency to build 51

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.51-beta")
 }
 
 java {
